ad650ac045a0b15d34ab876bd2a45b1acfd1537b
sparalaux <theodeletang@gmail.com>
2024-09-04 00:17:32 +0200

    retravail changement d'image

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var prix1 = 10;
 var prix2 = 30;
 click.addEventListener("click", () => {
     compteur+=clicksouris;
-    score.innerHTML = "score: "+compteur;
+    score.innerHTML = "score: "+Math.ceil(compteur);
 
 });
 
@@ -22,7 +22,7 @@ shopitem1.addEventListener("click", () =>{
     if(compteur>=prix1){
         compteur-=prix1;
         Math.ceil(prix1 = prix1*1.2);
-        score.innerHTML = "score: "+compteur;
+        score.innerHTML = "score: "+Math.ceil(compteur);
         setInterval(()=>{
             compteur+=1;
             score.innerHTML = "score: "+compteur;
@@ -34,11 +34,12 @@ shopitem2.addEventListener("click",()=>{
     if(compteur>=prix2){
         compteur-=prix2;
         Math.ceil(prix2 = prix2*1.2);
-        score.innerHTML = "score: "+compteur;
+        score.innerHTML = "score: "+Math.ceil(compteur);
         clicksouris+=1;
-        if(click.outerHTML == '<img id="click" src="./assets/image/oeuf_axolotl.jpg" alt="">'){
-            click.outerHTML = '<img id="click" src="./assets/image/bébé_axolotl.jpg" alt="">';
+        if(click.currentSrc == "https://sparalaux.github.io/clicker_game/assets/image/oeuf_axolotl.jpg"){
+            click.currentSrc = "https://sparalaux.github.io/clicker_game/assets/image/bébé_axolotl.jpg";
             console.log("image changer");
+            console.log(click);
         }
     }
 });
