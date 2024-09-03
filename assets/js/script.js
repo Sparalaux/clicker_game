b88bc0c244c1189cd3cde15dc1e5a422333ba442
sparalaux <theodeletang@gmail.com>
2024-09-03 16:15:31 +0200

    premier item du shop fonctionel ajout de point via setinterval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 const click = document.querySelector("#click");
 const score = document.querySelector("#score");
+const shopitem1 = document.querySelector("#shopitem1")
 console.log(click);
 console.log(score);
+
+
+
+
 var compteur = 0;
+
 click.addEventListener("click", () => {
     compteur+=1;
     score.innerHTML = "score: "+compteur;
 
+})
+
+shopitem1.addEventListener("click", () =>{
+    if(compteur>=10){
+        compteur-=10;
+        score.innerHTML = "score: "+compteur;
+        setInterval(()=>{
+            compteur+=1;
+            score.innerHTML = "score: "+compteur;
+        }, 1000)
+    }
 })
